6e70dd25065fb307a44c0e4b7736212ceddcfaa8
Sergey Polyakov <nazaruk-sergei@mail.ru>
2019-05-27 21:38:20 +0300

    InitialCommit

---
+++ src/com/sergo/helloworld/HelloIDEA.java
@@ -0,0 +1,17 @@
+package com.sergo.helloworld;
+
+import javax.swing.JFrame;
+
+public class HelloIDEA extends JFrame{
+
+    public HelloIDEA(){
+        super("HelloIdeaApp!");
+        setBounds(100,100,640,480);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+	    System.out.println("hello!");// write your code here
+        HelloIDEA appObj = new HelloIDEA();
+        appObj.setVisible(true);
+    }
+}
